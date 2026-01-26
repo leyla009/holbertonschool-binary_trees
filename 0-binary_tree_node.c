89cca0cc5ae8262d10fceedf414e6e0f8c439441
Leyla <Leyla.khaspolad@gmail.com>
2026-01-26 09:56:35 +0000

    Fix the typo

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,7 +3,7 @@
 
 /**
  * binary_tree_node - Creates a binary tree node
- * @parent: Pointer to the parent node 
+ * @parent: Pointer to the parent node
  * @value: Value to put in the new node
  * Return: Pointer to the new node, or NULL on failure
  */
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
-	
+
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
