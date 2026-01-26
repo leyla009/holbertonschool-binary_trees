2c6a1f710a2c10b0dfc1423a244006868e10a432
Leyla <Leyla.khaspolad@gmail.com>
2026-01-26 14:23:23 +0000

    Fix 132-array_to_heap.c Betty Style

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -5,7 +5,8 @@
  * @array: Pointer to the first element of the array to be converted
  * @size: The number of elements in the array
  *
- * Return: Pointer to the root node of the created Binary Heap, or NULL on failure
+ * Return: Pointer to the root node of the created Binary Heap,
+ * or NULL on failure
  */
 heap_t *array_to_heap(int *array, size_t size)
 {
